b9ba1d10acb460cb097a6a89146a707d887c9ca7
Tony Bowden <tony@tmtm.com>
2018-06-25 14:09:00 +0100

    tidy scraper

--- scraper.rb
+++ scraper.rb
@@ -1,18 +1,12 @@
 #!/bin/env ruby
-# encoding: utf-8
+# frozen_string_literal: true
 
-require 'scraperwiki'
-require 'nokogiri'
 require 'pry'
+require 'scraped'
+require 'scraperwiki'
 
 require 'open-uri/cached'
 OpenURI::Cache.cache_path = '.cache'
-
-class String
-  def tidy
-    self.gsub(/[[:space:]]+/, ' ').strip
-  end
-end
 
 def noko_for(url)
   Nokogiri::HTML(open(url).read)
@@ -21,16 +15,17 @@ end
 def scrape_list(url)
   noko = noko_for(url)
   noko.xpath('//h2[span[@id="Members"]]/following-sibling::ul[1]/li').each do |li|
-    data = { 
-      name: li.at_css('a').text.tidy,
-      honorific_prefix: "Cardinal",
-      wikiname: li.at_xpath('.//a[not(@class="new")][1]/@title').text,
-      party_id: "_ind",
-      party: "n/a",
-      term: 2016,
+    data = {
+      name:             li.at_css('a').text.tidy,
+      honorific_prefix: 'Cardinal',
+      wikiname:         li.at_xpath('.//a[not(@class="new")][1]/@title').text,
+      party_id:         '_ind',
+      party:            'n/a',
+      term:             2016,
     }
-    ScraperWiki.save_sqlite([:name, :term], data)
+    puts data.reject { |_, v| v.to_s.empty? }.sort_by { |k, _| k }.to_h if ENV['MORPH_DEBUG']
+    ScraperWiki.save_sqlite(%i[name term], data)
   end
 end
 
-scrape_list("https://en.wikipedia.org/wiki/Pontifical_Commission_for_Vatican_City_State")
+scrape_list('https://en.wikipedia.org/wiki/Pontifical_Commission_for_Vatican_City_State')
