7995ac600c9fee6cf4d7c52ed26187e2a14f767f
Tony Bowden <tony@tmtm.com>
2018-06-25 14:14:27 +0100

    Migrate to Scraped

--- scraper.rb
+++ scraper.rb
@@ -8,26 +8,39 @@ require 'scraperwiki'
 require 'open-uri/cached'
 OpenURI::Cache.cache_path = '.cache'
 
-def noko_for(url)
-  Nokogiri::HTML(open(url).read)
-end
-
-def scrape_list(url)
-  noko = noko_for(url)
-  noko.xpath('//h2[span[@id="Members"]]/following-sibling::ul[1]/li').map do |li|
-    {
-      name:             li.at_css('a').text.tidy,
-      honorific_prefix: 'Cardinal',
-      wikiname:         li.at_xpath('.//a[not(@class="new")][1]/@title').text,
-      party_id:         '_ind',
-      party:            'n/a',
-      term:             2016,
-    }
+class MembersPage < Scraped::HTML
+  field :members do
+    noko.xpath('//h2[span[@id="Members"]]/following-sibling::ul[1]/li').map do |li|
+      fragment(li => MemberRow).to_h
+    end
   end
 end
 
-data = scrape_list('https://en.wikipedia.org/wiki/Pontifical_Commission_for_Vatican_City_State')
-data.each { |mem| puts mem.reject { |_, v| v.to_s.empty? }.sort_by { |k, _| k }.to_h } if ENV['MORPH_DEBUG']
+class MemberRow < Scraped::HTML
+  field :name do
+    noko.at_css('a').text.tidy
+  end
 
-ScraperWiki.sqliteexecute('DROP TABLE data') rescue nil
-ScraperWiki.save_sqlite(%i[name term], data)
+  field :honorific_prefix do
+    'Cardinal'
+  end
+
+  field :wikiname do
+    noko.at_xpath('.//a[not(@class="new")][1]/@title').text
+  end
+
+  field :party_id do
+    '_ind'
+  end
+
+  field :party do
+    'n/a'
+  end
+
+  field :term do
+    2016
+  end
+end
+
+url = 'https://en.wikipedia.org/wiki/Pontifical_Commission_for_Vatican_City_State'
+Scraped::Scraper.new(url => MembersPage).store(:members, index: %i[name term])
