4b76cc75785fadbea97e506ee9a95b59ac28528b
Tony Bowden <tony@tmtm.com>
2018-06-25 14:10:23 +0100

    gather data and insert in one shot

--- scraper.rb
+++ scraper.rb
@@ -14,8 +14,8 @@ end
 
 def scrape_list(url)
   noko = noko_for(url)
-  noko.xpath('//h2[span[@id="Members"]]/following-sibling::ul[1]/li').each do |li|
-    data = {
+  noko.xpath('//h2[span[@id="Members"]]/following-sibling::ul[1]/li').map do |li|
+    {
       name:             li.at_css('a').text.tidy,
       honorific_prefix: 'Cardinal',
       wikiname:         li.at_xpath('.//a[not(@class="new")][1]/@title').text,
@@ -23,9 +23,11 @@ def scrape_list(url)
       party:            'n/a',
       term:             2016,
     }
-    puts data.reject { |_, v| v.to_s.empty? }.sort_by { |k, _| k }.to_h if ENV['MORPH_DEBUG']
-    ScraperWiki.save_sqlite(%i[name term], data)
   end
 end
 
-scrape_list('https://en.wikipedia.org/wiki/Pontifical_Commission_for_Vatican_City_State')
+data = scrape_list('https://en.wikipedia.org/wiki/Pontifical_Commission_for_Vatican_City_State')
+data.each { |mem| puts mem.reject { |_, v| v.to_s.empty? }.sort_by { |k, _| k }.to_h } if ENV['MORPH_DEBUG']
+
+ScraperWiki.sqliteexecute('DROP TABLE data') rescue nil
+ScraperWiki.save_sqlite(%i[name term], data)
